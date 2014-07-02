902cb46d6e5b62c267d9a975a87bf8df00fb709d
Daniel <daniel@courbanize.com>
2014-07-02 17:46:08 +0100

    version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-wms',
-    version='0.1',
+    version='0.1.1',
     packages=['wms'],
     include_package_data=True,
     license='BSD',
