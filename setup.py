9aff61fc07d85ca718669c2ccfba3543b59603db
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-04-26 12:35:12 +0100

    Bumped version number, improved setup.py.

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
-import os
-
 from setuptools import setup
-
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-wms',
-    version='0.1.11',
+    version='0.1.12',
     packages=['wms'],
     include_package_data=True,
     license='BSD',
     description='Mapscript based WMS framework for GeoDjango',
-    long_description=README,
-    url='https://github.com/geodesign/django-raster',
-    download_url='https://github.com/geodesign/django-wms/tarball/v0.1.11',
+    url='https://github.com/geodesign/django-wms',
     author='Daniel Wiesmann',
     author_email='daniel@urbmet.com',
-    keywords=['django', 'gis', 'mapserver', 'mapscript', 'wms',
-              'tms', 'web map service', 'tile map service'],
+    install_requires=[
+        'Django>=1.9',
+    ],
+    keywords=[
+        'django', 'gis', 'mapserver', 'mapscript', 'wms',
+        'tms', 'web map service', 'tile map service',
+    ],
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+        'Topic :: Scientific/Engineering :: GIS',
+    ]
 )
